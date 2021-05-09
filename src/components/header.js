52112926c581b6d1b9547fd027a5f92152cc45c7
Si Jobling <si@jobling.com>
2021-05-09 21:25:56 +0100

    Show logo in header

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,14 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import "./header.css"
+import logo from "../static/football-cal-128.png"
 
 const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1>
-        <Link
-          to="/"
-        >
-          {siteTitle}
+        <Link to="/">
+          <img src={logo} alt={siteTitle} height="48" />
         </Link>
       </h1>
     </div>
