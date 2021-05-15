9a595b07b60a36959f123c5b17138e3475e6a58e
Si Jobling <si@jobling.com>
2021-05-15 22:48:21 +0100

    Set ICS hostname dynamically

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -1,11 +1,13 @@
 //import { Link } from "gatsby"
 import React from "react"
+var os = require("os");
+var siteAddress = os.hostname();
 
 const Subscribe = ({ tag }) => (
   <div>
   		<p>
         <span role="img" aria-label="Spiral calendar">🗓</span> 
-        <a href={"webcal://footballcal.com/" + (tag ? tag.toLowerCase().replace(" ", "-") : "events") + ".ics"} className="ics">Subscribe to {tag || "all"} games in iOS, MacOS and Office</a>
+        <a href={"webcal://" + siteAddress + "/" + (tag ? tag.toLowerCase().replace(" ", "-") : "events") + ".ics"} className="ics">Subscribe to {tag || "all"} games in iOS, MacOS and Office</a>
         <small
           style={{
             display: `block`,
