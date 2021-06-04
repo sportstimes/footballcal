1aeae0f94164b9c1fd0b49eefade69f08b3c0021
Si Jobling <si@jobling.com>
2021-06-04 14:35:33 +0100

    Set OG and meta elements in Helment (Head)

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,18 +10,25 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
 import favicon from '../static/footballcal-32.png'
+import imageUrl from '../static/footballcal-opengraph.png'
 import Helmet from 'react-helmet'
 
 import Header from "./header"
 import "./layout.css"
 
 
-const Layout = ({ children }) => {
+const Layout = ({ children, type }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
           title
+          description
+          author
+          social {
+            twitterUsername
+            twitter
+          } 
         }
       }
     }
@@ -31,7 +38,24 @@ const Layout = ({ children }) => {
     <div>
       <Helmet>
         <link rel="icon" href={favicon} />
+​
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:site" contact={data.site.siteMetadata.social.twitterUsername}></meta>
+        <meta name="twitter:creator" content={data.site.siteMetadata.twitterUsername}></meta>
+        <meta name="twitter:title" content={data.site.siteMetadata.title}></meta>
+        <meta name="twitter:description" content={data.site.siteMetadata.description}></meta>
+        <meta name="twitter:image" content={imageUrl}></meta>
+​
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:site_name" content={data.site.siteMetadata.title} />
+        <meta property="og:title" content={data.site.siteMetadata.title}></meta>
+        <meta property="og:url" content={data.site.siteMetadata.siteUrl}></meta>
+        <meta property="og:description" content={data.site.siteMetadata.description}></meta>
+        <meta property="og:image" content={imageUrl}></meta>
+        <meta property="og:image:alt" content={data.site.siteMetadata.description}></meta>
+        <meta property="og:type" content={type} />
       </Helmet>
+      
       <Header siteTitle={data.site.siteMetadata.title} />
       <div id="content">
         <main>{children}</main>
