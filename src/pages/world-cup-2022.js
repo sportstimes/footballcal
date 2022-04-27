07d600ae86611369c78ee3998947939148e83ddb
Si Jobling <si@jobling.com>
2022-04-27 22:14:44 +0100

    Parse WC22 event data and output teams

--- src/pages/world-cup-2022.js
+++ src/pages/world-cup-2022.js
@@ -1,17 +1,40 @@
 import React from "react"
 import JSONData from "../content/bbc-world-cup-data.json"
 
-const fixtures = Object.entries(JSONData.payload[0].body.matchData[0].tournamentDatesWithEvents);
+const events = Object.values(JSONData.payload[0].body.matchData[0].tournamentDatesWithEvents).flatMap(event => event.map(subEvent => subEvent.events)).flatMap(event => event).map(event => (
+  {
+      startTime: event.startTime, 
+      homeTeam: event.homeTeam.name.full, 
+      awayTeam: event.awayTeam.name.full, 
+      venue: event.venue.name.full
+  }
+))
+/*
+.reduce((acc, event) => {
+  const date = event.startTime.split("T")[0];
+  if (date in acc) {
+      acc[date].push(event)
+  } else {
+      acc[date] = [event];
+  }
+  return acc;
+}, {})
+*/
+
 
 const WorldCup2022 = () => (
 
   <div>
-    <h1>{ JSONData.payload[0].body.matchData[0].tournamentMeta.tournamentName.full }</h1>
+    <h1>{ JSONData.payload[0].body.matchData[0].tournamentMeta.tournamentName.full}</h1>
     <ol>
+
     {
-      fixtures.map((data, index) => {
-        return <li key={`content_item_${index}`}>{data.events}</li>
+      
+      events.map((event, index) => {
+        return <li key={`content_item_${index}`}>{event.homeTeam + ' v ' + event.awayTeam}</li>
+        
       })
+      
     }
     </ol>
   </div>
